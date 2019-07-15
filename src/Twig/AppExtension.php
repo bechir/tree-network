817f521163136386373a149f7658889c9a6d6e3c
rand0mdev <bechirr71@gmail.com>
2019-07-15 01:52:50 +0000

    Setup languages

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Twig;
+
+use Symfony\Component\Intl\Intl;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class AppExtension extends AbstractExtension
+{
+    private $localeCodes;
+    private $locales;
+    private $container;
+
+    public function __construct(ContainerInterface $container, string $locales)
+    {
+        $this->container = $container;
+        $this->localeCodes = explode('|', $locales);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters(): array
+    {
+        return [
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('locales', [$this, 'getLocales']),
+        ];
+    }
+
+    /**
+     * Takes the list of codes of the locales (languages) enabled in the
+     * application and returns an array with the name of each locale written
+     * in its own language (e.g. English, Français, Español, etc.).
+     */
+    public function getLocales(): array
+    {
+        if (null !== $this->locales) {
+            return $this->locales;
+        }
+
+        $this->locales = [];
+        foreach ($this->localeCodes as $localeCode) {
+            $this->locales[] = ['code' => $localeCode, 'name' => Intl::getLocaleBundle()->getLocaleName($localeCode, $localeCode)];
+        }
+
+        return $this->locales;
+    }
+}
