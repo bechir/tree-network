736c39ad0e2c4c45378348468d770ca159129e6b
rand0mdev <bechirr71@gmail.com>
2019-07-19 18:28:07 +0000

    Translate login page

---
+++ templates/tree-family/contact_modal.js
@@ -0,0 +1,17 @@
+var subjects = [
+    "Where's My Stuff?",
+    "Cancel Items or Orders",
+    "Returns & Refunds",
+    "Shipping Rates & Information",
+    "Change Your Payment Method",
+    "Manage Your Account Information",
+    "About Two-Step Verification",
+    "Cancel Items or Orders",
+    "Change Your Order Information",
+    "Contact Third-Party Sellers",
+    "More in Managing Your Orders"
+    ];
+    
+    $('#form-autocomplete-2').mdbAutocomplete({
+    data: subjects
+    });
